bc0cb347c47da89c6563fb2d7cbc14f26148be16
Álvaro <alvaro8038c@gmail.com>
2025-03-13 14:37:36 +0100

    p2 terminada

--- sql/ud5/practica2.sql
+++ sql/ud5/practica2.sql
@@ -5,7 +5,7 @@ USE practica2_ud5;
 CREATE TABLE almacenes (
   codigo INT PRIMARY KEY,
   lugar VARCHAR(100),
-  capacidad INT UNSIGNED,
+  capacidad INT UNSIGNED
 );
 
 CREATE TABLE cajas (
@@ -14,4 +14,39 @@ CREATE TABLE cajas (
     valor INT UNSIGNED,
     almacen INT,
     FOREIGN KEY (almacen) REFERENCES almacenes(codigo)
+);
+
+/* Apartado 1 */
+INSERT INTO almacenes
+VALUES 
+(1, 'Barcelona', 3);
+
+/* Apartado 2 */
+INSERT INTO cajas
+VALUES
+('H5RT','Papel',200, 2);
+
+/* Apartado 3 */
+UPDATE cajas
+SET valor=valor*0.85;
+
+/* Apartado 4 */
+UPDATE cajas
+SET valor=0.80*(
+  SELECT AVG(valor)
+  FROM cajas
+);
+
+/* Apartado 5 */
+DELETE
+FROM cajas
+WHERE valor < 100;
+
+/* Apartado 6 */
+UPDATE almacenes
+SET capacidad=0
+WHERE capacidad<(
+  SELECT COUNT(*)
+  FROM cajas
+  WHERE cajas.almacen = almacenes.codigo
 );
