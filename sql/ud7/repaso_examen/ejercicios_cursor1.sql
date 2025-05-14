282f410f8a4b654a059bc46edd2af3b1f591179b
Álvaro <alvaro8038c@gmail.com>
2025-05-14 23:26:07 +0200

    repaso examen ud7

--- sql/ud7/repaso_examen/ejercicios_cursor1.sql
+++ sql/ud7/repaso_examen/ejercicios_cursor1.sql
@@ -19,7 +19,7 @@ BEGIN
   bucle: LOOP
     FETCH cursor1 INTO vid; 
     IF lrf=1 THEN
-      LEAVE bucle
+      LEAVE bucle;
     END IF;
     SET vcontador = vcontador + 1;
   END LOOP bucle; 
@@ -29,3 +29,37 @@ BEGIN
 END; $$
 
 DELIMITER ;
+
+-- 2.
+DELIMITER $$
+USE bd_repaso $$
+DROP PROCEDURE IF EXISTS cuentas_saldo_negativo $$
+CREATE PROCEDURE IF cuentas_saldo_negativo()
+BEGIN
+  DECLARE lrf BOOLEAN;
+  DECLARE vid, vcontador INT;
+  DECLARE vnombre_titular VARCHAR(30);
+  DECLARE vfecha_apertura DATE;
+
+  DECLARE cursor1 CURSOR FOR
+  SELECT id INTO vid
+  FROM tabla
+  WHERE saldo < 0;
+
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET lrf=1;
+
+  SET vid=0, vcontador=0, vnombre_titular="", vfecha_apertura="", lrf=0;
+  
+  OPEN cursor1;
+  bucle: LOOP
+    FETCH into vid, vnombre_titular, vfecha_apertura;
+    IF lrf=1 THEN
+      LEAVE bucle;
+    END IF;
+    SELECT CONCAT('La cuenta con id ',id,' tiene de nombre titular a ',vnombre_titular,' y se abrió en la fecha ',vfecha_apertura);
+  END LOOP bucle;
+  CLOSE cursor1;
+
+  
+
+
