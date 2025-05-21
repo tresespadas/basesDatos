4e6fa5bdfe493851138fc2c486225c2b9d6d3ab0
Álvaro <alvaro8038c@gmail.com>
2025-05-21 16:58:03 +0200

    ej ud8

--- sql/ud8/act2.sh
+++ sql/ud8/act2.sh
@@ -9,8 +9,16 @@ BACKUP_FILE="$BACKUP_DIR/$DB_NAME-$DATE.sql"
 # Crear directorio de respaldo si no existe
 # mkdir -p $BACKUP_DIR
 
+# Iniciar el servicio
+sudo /opt/lampp/lampp start 2>/dev/null
+
+sleep 5
+
 # Ejecutar la copia
 mysqldump -u $DB_USER $DB_NAME > $BACKUP_FILE
+
+# Comprimir el archivo de respaldo
+gzip $BACKUP_FILE
 
 # Eliminar archivos de respaldo que fueron modificados hace más de 7 días
 find $BACKUP_DIR -type f -name "$DB_NAME*.sql.gz" -mtime +7 -exec rm {} \;
